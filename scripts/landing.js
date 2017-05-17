e320febfa2afecb2d7f0e2645bf4e03ad6aca867
jmuldvp <jmul.dvp@gmail.com>
2017-05-17 07:21:35 -0600

    CP22 assignment completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -3,7 +3,7 @@ var animatePoints = function() {
   var points = document.getElementsByClassName('point');
   
   var revealPoint = function(points) {
-    for(i = 0; i <= points.length; i++) {
+    for(i = 0; i <= points.length - 1; i++) {
       points[i].style.opacity = 1;
       points[i].style.transform = "scaleX(1) translateY(0)";
       points[i].style.msTransform = "scaleX(1) translateY(0)";
