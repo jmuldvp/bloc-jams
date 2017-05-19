203a189565fe501f144f90ce8e6bab4e74a65db2
jmuldvp <jmul.dvp@gmail.com>
2017-05-18 21:39:13 -0600

    WIP

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,9 @@
-var animatePoints = function() {
-        
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
+
   var points = document.getElementsByClassName('point');
-  
+
   var revealPoint = function(points) {
     for(i = 0; i <= points.length - 1; i++) {
       points[i].style.opacity = 1;
@@ -10,7 +12,21 @@ var animatePoints = function() {
       points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
     }
   };
-  
+
   revealPoint(points);
- 
+
 };
+
+window.onload = function() {
+  if (window.innerHeight > 950) {
+    animatePoints(pointsArray);
+  }
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+  window.addEventListener('scroll', function(event) {
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+      animatePoints(pointsArray);
+    }
+  });
+}
