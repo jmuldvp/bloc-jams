47ee47b29084b2b2d7216d8254f3428e373b034e
jmuldvp <jmul.dvp@gmail.com>
2017-07-10 23:13:29 -0600

    CP 31 completed

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,5 +1,5 @@
 var buildCollectionItemTemplate = function() {
-    '<div class="collection-album-container column fourth">'
+  var template =  '<div class="collection-album-container column fourth">'
   + '  <img src="assets/images/album_covers/01.png"/>'
   + '  <div class="collection-album-info caption">'
   + '    <p>'
